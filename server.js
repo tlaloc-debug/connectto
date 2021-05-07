b85bb38e3a5b5fd2bb538af62a46ea44eb60b6a2
tlaloc_debug <pelondinho@hotmail.com>
2021-05-07 17:50:52 -0400

    add headers

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app=express();
 let name;
 let picName;
 
-app.use(cors({origin: '*'}));
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(bodyparser.json());
 
 config.ssl = {
